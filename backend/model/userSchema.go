9652586db1d6575ebd7e1e1baa0318a5d5cb6566
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-09 12:07:44 +0530

    b4 adding jwt backend

--- backend/model/userSchema.go
+++ backend/model/userSchema.go
@@ -1,5 +1,15 @@
 package model
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type User struct {
-	Name string `json:"name" bson:"name"`
+	ID          primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	Username    string               `bson:"username" json:"username" validate:"required,min=3,max=30"`
+	Email       string               `bson:"email" json:"email" validate:"required,email"`
+	Description string               `bson:"description,omitempty" json:"description,omitempty"`
+	Following   []primitive.ObjectID `bson:"following,omitempty" json:"following,omitempty"`
+	Followers   []primitive.ObjectID `bson:"followers,omitempty" json:"followers,omitempty"`
+	Websites    []primitive.ObjectID `bson:"websites,omitempty" json:"websites,omitempty"`
 }
