4b13021e7061c76ffdfd1e44fc343e32791c4384
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-09 16:45:30 +0530

    jwt signUp

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/AKHIL-GIREESH/Webweaver/model"
 	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -16,21 +19,35 @@ func NewJWTService(config model.JWTConfig) *model.JWTService {
 
 func SignUp(c fiber.Ctx, collection *mongo.Collection) error {
 
-	var users []model.User
-	cursor, err := collection.Find(context.Background(), nil)
+	jwtService := &model.JWTService{
+		Config: model.JWTConfig{
+			TokenSecret: os.Getenv("SECRET_KEY"),
+			TokenExp:    24 * time.Hour,
+		},
+	}
+
+	user := new(model.User)
+	if err := c.Bind().JSON(user); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
+	}
+
+	insertUser, err := collection.InsertOne(context.Background(), user)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": "Failed to retrieve users"})
-	}
-	defer cursor.Close(context.Background())
-
-	for cursor.Next(context.Background()) {
-		var user model.User
-		if err := cursor.Decode(&user); err != nil {
-			return c.Status(500).JSON(fiber.Map{"error": "Error decoding user data"})
-		}
-		users = append(users, user)
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to insert user"})
 	}
 
-	return c.JSON(users)
+	user.ID, _ = insertUser.InsertedID.(primitive.ObjectID)
+
+	token, err := jwtService.GenerateToken(user)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to generate token",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"token": token,
+		"user":  user,
+	})
 
 }
