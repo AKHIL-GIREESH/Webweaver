ae25c400cbb015701f9e7d56170e7f2be3819a34
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-10 06:32:30 +0530

    changed user and website schema

--- backend/model/userSchema.go
+++ backend/model/userSchema.go
@@ -4,13 +4,20 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type FollowUser struct {
+	ID         primitive.ObjectID `bson:"id" json:"id"`
+	Username   string             `bson:"username" json:"username"`
+	ProfilePic string             `bson:"profile_pic" json:"profile_pic"`
+}
+
 type User struct {
-	ID          primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
-	Username    string               `bson:"username" json:"username" validate:"required,min=3,max=30"`
-	Email       string               `bson:"email" json:"email" validate:"required,email"`
-	Password    string               `bson:"password" json:"password" validate:"required,min=3,max=30"`
-	Description string               `bson:"description,omitempty" json:"description,omitempty"`
-	Following   []primitive.ObjectID `bson:"following,omitempty" json:"following,omitempty"`
-	Followers   []primitive.ObjectID `bson:"followers,omitempty" json:"followers,omitempty"`
-	Websites    []primitive.ObjectID `bson:"websites,omitempty" json:"websites,omitempty"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username    string             `bson:"username" json:"username" validate:"required,min=3,max=30"`
+	Email       string             `bson:"email" json:"email" validate:"required,email"`
+	Password    string             `bson:"password" json:"password" validate:"required,min=3,max=30"`
+	Description string             `bson:"description,omitempty" json:"description,omitempty"`
+	ProfilePic  string             `bson:"profile_pic,omitempty" json:"profile_pic,omitempty"`
+	Following   []FollowUser       `bson:"following,omitempty" json:"following,omitempty"`
+	Followers   []FollowUser       `bson:"followers,omitempty" json:"followers,omitempty"`
+	Websites    []WebsiteUser      `bson:"websites,omitempty" json:"websites,omitempty"`
 }
--- backend/model/websiteSchema.go
+++ backend/model/websiteSchema.go
@@ -5,5 +5,16 @@ import (
 )
 
 type Website struct {
-	ID primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title     string             `bson:"title" json:"title"`
+	Thumbnail string             `bson:"thumbnail" json:"thumbnail"`
+	Author    FollowUser         `bson:"author,omitempty" json:"author,omitempty"`
+	Like      int                `bson:"like" json:"like"`
+	// Tags	string
+}
+
+type WebsiteUser struct {
+	ID        primitive.ObjectID `bson:"id" json:"id"`
+	Title     string             `bson:"title" json:"title"`
+	Thumbnail string             `bson:"thumbnail" json:"thumbnail"`
 }
