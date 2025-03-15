0404c5830f5962e85d0a994699bf03fa68a6ba62
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-15 07:29:42 +0530

    modified follow, following and website display schema

--- backend/controllers/userController.go
+++ backend/controllers/userController.go
@@ -60,18 +60,6 @@ func GetSelf(c fiber.Ctx, collection *mongo.Collection) error {
 	})
 }
 
-// func CreateUser(c fiber.Ctx, collection *mongo.Collection) error {
-// 	user := new(model.User)
-// 	if err := c.Bind().JSON(user); err != nil {
-// 		return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
-// 	}
+func FollowUser(c fiber.Ctx, collection *mongo.Collection) {
 
-// 	insertUser, err := collection.InsertOne(context.Background(), user)
-// 	if err != nil {
-// 		return c.Status(500).JSON(fiber.Map{"error": "Failed to insert user"})
-// 	}
-
-// 	user.ID, _ = insertUser.InsertedID.(primitive.ObjectID)
-
-// 	return c.Status(200).JSON(user)
-// }
+}
