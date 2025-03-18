ff69a7fa5fe648f16a07b025499b22c1f61636dd
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-18 10:32:58 +0530

    file Upload to S3

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -8,6 +10,11 @@ import (
 	"github.com/AKHIL-GIREESH/Webweaver/routes"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
@@ -21,6 +28,57 @@ func main() {
 
 	routes.AuthRoutes(app, userCollection)
 	routes.UserRoutes(app, userCollection)
+
+	app.Post("/upload", func(c fiber.Ctx) error {
+		file, err := c.FormFile("upload")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Failed to retrieve file",
+			})
+		}
+
+		fmt.Println("hi")
+
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Failed to retrieve file",
+			})
+		}
+
+		uploadFile, err := file.Open()
+		if err != nil {
+			log.Printf("error: %v", err)
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Failed to retrieve file",
+			})
+		}
+
+		client := s3.NewFromConfig(cfg)
+
+		uploader := manager.NewUploader(client)
+		result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+			Bucket:      aws.String("webweaver-users"),
+			Key:         aws.String(file.Filename),
+			Body:        uploadFile,
+			ContentType: aws.String("image/jpeg"),
+			ACL:         "public-read",
+		})
+
+		if err != nil {
+			log.Printf("error: %v", err)
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Failed to retrieve file",
+			})
+		}
+
+		fmt.Println(result)
+
+		return c.Status(200).JSON(fiber.Map{
+			"imgUrl": result.Location,
+		})
+	})
 
 	log.Fatal(app.Listen(":3000"))
 }
