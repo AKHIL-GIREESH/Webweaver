094cf57e32130a8f8052524a1dd2922ca87964ae
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-19 14:59:08 +0530

    edit profileui done

--- frontend/src/components/Profile/EditImg.tsx
+++ frontend/src/components/Profile/EditImg.tsx
@@ -6,14 +6,14 @@ const EditImg = ({ banner, elem }: { banner: boolean, elem: string | undefined }
 
     const [file, setFile] = useState<File | null>(null)
 
-    elem = "https://fontmeme.com/images/set-in-friz-bold.png"
+    //elem = "https://fontmeme.com/images/set-in-friz-bold.png"
 
     return (
         <>
-            <p className="font-semibold text-[1.2rem]">{banner ? "BANNER" : "DISPLAY PICTURE"}</p>
-            <div className={`mb-[5vh] flex justify-center items-center ${banner ? `w-[95%] min-h-[22vh] rounded-[10px] ${elem ? `bg-[url(${elem})] cover` : "bg-black"}` : ""}`}>
-                <div className=" flex justify-center items-center relative rounded-full w-[50px] h-[50px]  bg-gradient-to-br from-[#ffd700] via-[#f0c14b] to-[#b8860b]">
-                    <input type="file" className=" hover:cursor-pointer absolute rounded-full w-[50px] h-[50px] opacity-0" />
+            <p className="font-semibold text-[1.2rem] mb-[3vh]">{banner ? "BANNER" : "DISPLAY PICTURE"}</p>
+            <div className={`mb-[5vh] flex justify-center items-center ${banner ? `w-[95%] min-h-[22vh] rounded-[10px] ${elem ? `bg-[url(${elem})] cover` : "bg-black"}` : `min-h-[150px] w-[150px] rounded-full ${elem ? `bg-[url(${elem})] cover` : "bg-black"}`}`}>
+                <div className=" flex justify-center items-center relative rounded-full w-[45px] h-[45px]  bg-gradient-to-br from-[#ffd700] via-[#f0c14b] to-[#b8860b]">
+                    <input type="file" className=" hover:cursor-pointer absolute rounded-full w-[45px] h-[45px] opacity-0" />
                     <Pencil className="text-black" />
                 </div>
             </div>
