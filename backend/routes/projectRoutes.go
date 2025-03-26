33c016d3260a68a209d7d086bd4bd62fd931de1b
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-26 21:39:55 +0530

    project backend

--- backend/routes/projectRoutes.go
+++ backend/routes/projectRoutes.go
@@ -2,14 +2,13 @@ package routes
 
 import (
 	"github.com/AKHIL-GIREESH/Webweaver/controllers"
-	"github.com/AKHIL-GIREESH/Webweaver/middlewares"
 	"github.com/gofiber/fiber/v3"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func ProjectRoutes(app *fiber.App, projectCollection *mongo.Collection) {
 
-	app.Get("/project/", middlewares.JWTCheckMiddleware(), func(c fiber.Ctx) error {
+	app.Get("/project/", func(c fiber.Ctx) error {
 		return controllers.GetAllProjects(c, projectCollection)
 	})
 	app.Post("/project/", func(c fiber.Ctx) error {
