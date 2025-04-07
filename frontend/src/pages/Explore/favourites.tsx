997276e98ee22a3cb8738486153d8c71ca492196
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-04-07 07:28:53 +0530

    favourite ui

--- frontend/src/pages/Explore/favourites.tsx
+++ frontend/src/pages/Explore/favourites.tsx
@@ -1,5 +1,10 @@
+import { getLikedProjects } from "@/api/getLikedProjects"
+import Errorr from "@/components/Layout/Errorr"
+import Loading from "@/components/Layout/Loading"
+import ProjectCard from "@/components/Layout/ProjectCard"
 import { Button } from "@/components/ui/button"
 import { AuthContext } from "@/providers/authProvider"
+import { useQuery } from "@tanstack/react-query"
 import { useContext } from "react"
 import { Link } from "react-router-dom"
 
@@ -11,18 +16,35 @@ const Favourite = () => {
         return <>Login to continue</>
     }
 
-    const { liked } = UserContext.user
+    const { liked, id, websites } = UserContext.user
 
-    if (!liked || liked?.length === 0) {
-        return (
-            <div>
-                <p>Handpicked By You</p>
-                You haven't liked any.<br />
-                <Link to="/explore"><Button variant="auth">Explore</Button></Link>
-            </div>
-        )
-    }
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["myfav"],
+        queryFn: async () => {
+            if (!id) throw new Error("No ID found")
+            const projects = await getLikedProjects(id)
+            console.log(projects)
+            return projects
+        },
+        enabled: !!id,
+    })
 
+    if (isLoading) return <Loading />
+    if (error || !data) return <Errorr />
+
+
+    return (
+        <div>
+            <p>Handpicked By You</p>
+            {(!liked || liked?.length === 0) && <p>You haven't liked any<br /></p>}
+            {(liked && liked?.length > 0) &&
+                data.map(({ _id, title }: { _id: string, title: string }) => <ProjectCard _id={_id as string} title={title} liked={true} self={websites?.includes(_id) ? true : false} />)
+            }
+            <Link to="/explore"><Button variant="auth">Explore</Button></Link>
+        </div>
+    )
 }
+
+
 
 export default Favourite
